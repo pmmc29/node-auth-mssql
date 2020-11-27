51791a626ec86f3277d2e9d15d6a58f18eff9304
pedro <pemimeco@gmail.com>
2020-11-27 15:09:14 -0400

    comprobante beneficiarios mayores a 19 v1.1

--- src/models/comprobante.js
+++ src/models/comprobante.js
@@ -142,34 +142,68 @@ async function verificarComprobanteB(req, res) {
                         }
                         const dias_rest = await requestdb.query(`SELECT DATEDIFF(day, DATEADD(mm, ${meses}, GETDATE()), 
                         DATEADD(yy, 25, (select CONVERT(date, (select fec_nac from beneficiarios where cod_bnf = '${req.body.codigo}'), 103)))) AS dias_rest`)
+                        const current_year = await requestdb.query(`SELECT DATEDIFF(day, DATEADD(mm, ${meses}, GETDATE()), CONCAT(YEAR(GETDATE()), '-12-31')) AS current_year`)
                         console.log(dias_rest.recordset[0].dias_rest)
-                        if (dias_rest.recordset[0].dias_rest >= 0) {//tiene dias sobrantes -> se registra con los meses especificados
-                            const imp_carnet = await requestdb.query(`insert into imp_carnet (id_carnet,front,back,fec_emision,estado,id_usuario,validez,comprobante,motivo,fec_fin) 
-                                                    values((select id_carnet from carnet where cod_bnf = '${req.body.codigo}'),'0','0', CONVERT(VARCHAR,GETDATE(), 103), '0',
-                                                    ${req.user.id}, '${req.body.validez}','${result.recordset[0].Numero}','${req.body.motivo}',CONVERT(VARCHAR, DATEADD(mm, ${meses}, GETDATE()), 103))`)
-                            if (imp_carnet.rowsAffected[0] === 1) { //1 fila afectada, se registro correctamente
-                                req.flash('loginMessage', `Comprobante: ${req.body.comprobante}, Concepto: ${result.recordset[0].Concepto}`)
-                                req.flash('aux', req.body.codigo)
-                                res.redirect('/buscarBeneficiario')
-                            } else {
-                                req.flash('loginMessage', 'Error en el registro del comprobante')
-                                req.flash('aux', req.body.codigo)
-                                res.redirect('/buscarBeneficiario')
+                        if (current_year.recordset[0].current_year >= 0) { //tiene dias sobrantes al año actual
+                            if (dias_rest.recordset[0].dias_rest >= 0) {//tiene dias sobrantes para sus 25 -> se registra con los meses especificados
+                                const imp_carnet = await requestdb.query(`insert into imp_carnet (id_carnet,front,back,fec_emision,estado,id_usuario,validez,comprobante,motivo,fec_fin) 
+                                                        values((select id_carnet from carnet where cod_bnf = '${req.body.codigo}'),'0','0', CONVERT(VARCHAR,GETDATE(), 103), '0',
+                                                        ${req.user.id}, '${req.body.validez}','${result.recordset[0].Numero}','${req.body.motivo}',CONVERT(VARCHAR, DATEADD(mm, ${meses}, GETDATE()), 103))`)
+                                if (imp_carnet.rowsAffected[0] === 1) { //1 fila afectada, se registro correctamente
+                                    req.flash('loginMessage', `Comprobante: ${req.body.comprobante}, Concepto: ${result.recordset[0].Concepto}`)
+                                    req.flash('aux', req.body.codigo)
+                                    res.redirect('/buscarBeneficiario')
+                                } else {
+                                    req.flash('loginMessage', 'Error en el registro del comprobante')
+                                    req.flash('aux', req.body.codigo)
+                                    res.redirect('/buscarBeneficiario')
+                                }
                             }
-                        }
-                        if (dias_rest.recordset[0].dias_rest < 0) {//no tiene dias restantes -> se limita la fecha de validez a su cumpleaños 25
-                            const new_fec_fin = `CONVERT(VARCHAR, (select DATEADD(yy, 25, (select CONVERT(date, (select fec_nac from beneficiarios where cod_bnf = '${req.body.codigo}'), 103)))), 103)`
-                            const imp_carnet = await requestdb.query(`insert into imp_carnet (id_carnet,front,back,fec_emision,estado,id_usuario,validez,comprobante,motivo,fec_fin) 
+                            if (dias_rest.recordset[0].dias_rest < 0) {
+                                const new_fec_fin = `CONVERT(VARCHAR, (select DATEADD(yy, 25, (select CONVERT(date, (select fec_nac from beneficiarios where cod_bnf = '${req.body.codigo}'), 103)))), 103)`
+                                const imp_carnet = await requestdb.query(`insert into imp_carnet (id_carnet,front,back,fec_emision,estado,id_usuario,validez,comprobante,motivo,fec_fin) 
                                                     values((select id_carnet from carnet where cod_bnf = '${req.body.codigo}'),'0','0', CONVERT(VARCHAR,GETDATE(), 103), '0',
                                                     ${req.user.id}, '${req.body.validez}','${result.recordset[0].Numero}','${req.body.motivo}',${new_fec_fin})`)
-                            if (imp_carnet.rowsAffected[0] === 1) { //1 fila afectada, se registro correctamente
-                                req.flash('loginMessage', `Comprobante: ${req.body.comprobante}, Concepto: ${result.recordset[0].Concepto}`)
-                                req.flash('aux', req.body.codigo)
-                                res.redirect('/buscarBeneficiario')
-                            } else {
-                                req.flash('loginMessage', 'Error en el registro del comprobante')
-                                req.flash('aux', req.body.codigo)
-                                res.redirect('/buscarBeneficiario')
+                                if (imp_carnet.rowsAffected[0] === 1) { //1 fila afectada, se registro correctamente
+                                    req.flash('loginMessage', `Comprobante: ${req.body.comprobante}, Concepto: ${result.recordset[0].Concepto}`)
+                                    req.flash('aux', req.body.codigo)
+                                    res.redirect('/buscarBeneficiario')
+                                } else {
+                                    req.flash('loginMessage', 'Error en el registro del comprobante')
+                                    req.flash('aux', req.body.codigo)
+                                    res.redirect('/buscarBeneficiario')
+                                }
+                            }
+                        }
+                        if (current_year.recordset[0].current_year < 0) { //no tiene dias restantes en el año actual -> se limita la fecha de validez al 31/12
+                            if (dias_rest.recordset[0].dias_rest < 0 && current_year.recordset[0].current_year < 0) { //no tiene dias restantes para sus 25 -> se limita a la fecha de su cumpleaños 25
+                                const new_fec_fin = `CONVERT(VARCHAR, (select DATEADD(yy, 25, (select CONVERT(date, (select fec_nac from beneficiarios where cod_bnf = '${req.body.codigo}'), 103)))), 103)`
+                                const imp_carnet = await requestdb.query(`insert into imp_carnet (id_carnet,front,back,fec_emision,estado,id_usuario,validez,comprobante,motivo,fec_fin) 
+                                                    values((select id_carnet from carnet where cod_bnf = '${req.body.codigo}'),'0','0', CONVERT(VARCHAR,GETDATE(), 103), '0',
+                                                    ${req.user.id}, '${req.body.validez}','${result.recordset[0].Numero}','${req.body.motivo}',${new_fec_fin})`)
+                                if (imp_carnet.rowsAffected[0] === 1) { //1 fila afectada, se registro correctamente
+                                    req.flash('loginMessage', `Comprobante: ${req.body.comprobante}, Concepto: ${result.recordset[0].Concepto}`)
+                                    req.flash('aux', req.body.codigo)
+                                    res.redirect('/buscarBeneficiario')
+                                } else {
+                                    req.flash('loginMessage', 'Error en el registro del comprobante')
+                                    req.flash('aux', req.body.codigo)
+                                    res.redirect('/buscarBeneficiario')
+                                }
+                            }else{
+                                const new_fec_fin = `CONCAT('31/12/',YEAR(GETDATE()))`
+                                const imp_carnet = await requestdb.query(`insert into imp_carnet (id_carnet,front,back,fec_emision,estado,id_usuario,validez,comprobante,motivo,fec_fin) 
+                                                        values((select id_carnet from carnet where cod_bnf = '${req.body.codigo}'),'0','0', CONVERT(VARCHAR,GETDATE(), 103), '0',
+                                                        ${req.user.id}, '${req.body.validez}','${result.recordset[0].Numero}','${req.body.motivo}',${new_fec_fin})`)
+                                if (imp_carnet.rowsAffected[0] === 1) { //1 fila afectada, se registro correctamente
+                                    req.flash('loginMessage', `Comprobante: ${req.body.comprobante}, Concepto: ${result.recordset[0].Concepto}`)
+                                    req.flash('aux', req.body.codigo)
+                                    res.redirect('/buscarBeneficiario')
+                                } else {
+                                    req.flash('loginMessage', 'Error en el registro del comprobante')
+                                    req.flash('aux', req.body.codigo)
+                                    res.redirect('/buscarBeneficiario')
+                                }
                             }
                         }
                     }
