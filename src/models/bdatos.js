5ccc6796e2d5a20c71ad45c272f66d6671f3c061
pedro <pemimeco@gmail.com>
2020-12-02 15:06:34 -0400

    actualizarBD v1.1

--- src/models/bdatos.js
+++ src/models/bdatos.js
@@ -2,9 +2,11 @@ const pool = require('../database')
 const sql = require('mssql')
 const QRCode = require('qrcode')
 const path = require('path');
-const csv = require('csvtojson')
+const csv = require('csvtojson');
 
 const poolConnect = pool.connect();
+const transaction = pool.transaction();
+const request = pool.request(transaction); // or: new sql.Request(pool1)
 
 pool.on('error', err => {
     // ... error handler
@@ -12,44 +14,85 @@ pool.on('error', err => {
 });
 
 
-async function updateAsegurados(req,res) {
+async function updateAsegurados(req, res) {
     try {
         await poolConnect; // ensures that the pool has been created
         const file_asegurados = `./src/dbfiles/new-asegurados2.txt`;
         const table = new sql.Table('test_ase')
         table.create = true
-        table.columns.add('agenda',sql.Int ,{nullable:false})
-        table.columns.add('cod_asegurado',sql.NVarChar(50) ,{nullable:false,primary:true})
-        table.columns.add('nombre',sql.NVarChar(50) ,{nullable:false})
-        table.columns.add('fec_nac',sql.NVarChar(50) ,{nullable:false})
-        table.columns.add('sexo',sql.Char(1) ,{nullable:false})
-        table.columns.add('ci',sql.NVarChar(50) ,{nullable:false})
-        table.columns.add('ci_loc',sql.NVarChar(50) ,{nullable:true})
-        table.columns.add('cod_emp',sql.NVarChar(50) ,{nullable:true})
-        table.columns.add('nom_emp',sql.Int ,{nullable:false})
-        table.columns.add('fec_ing',sql.NVarChar(50) ,{nullable:false})
-        table.columns.add('tipo_sangre',sql.NVarChar(10) ,{nullable:true})
-        const request = pool.request(); // or: new sql.Request(pool1)
+        table.columns.add('agenda', sql.Int, {
+            nullable: false
+        })
+        table.columns.add('cod_asegurado', sql.NVarChar(50), {
+            nullable: false,
+            primary: true
+        })
+        table.columns.add('nombre', sql.NVarChar(50), {
+            nullable: false
+        })
+        table.columns.add('fec_nac', sql.NVarChar(50), {
+            nullable: false
+        })
+        table.columns.add('sexo', sql.Char(1), {
+            nullable: false
+        })
+        table.columns.add('ci', sql.NVarChar(50), {
+            nullable: true
+        })
+        table.columns.add('ci_loc', sql.NVarChar(50), {
+            nullable: true
+        })
+        table.columns.add('cod_emp', sql.Int, {
+            nullable: false
+        })
+        table.columns.add('nom_emp', sql.NVarChar(50), {
+            nullable: false
+        })
+        table.columns.add('fec_ing', sql.NVarChar(50), {
+            nullable: false
+        })
+        table.columns.add('tipo_sangre', sql.NVarChar(10), {
+            nullable: true
+        })
         csv({
                 delimiter: ["|"],
                 headers: ['agenda', 'cod_asegurado', 'nombre', 'fec_nac', 'sexo', 'ci', 'ci_loc', 'cod_emp', 'nom_emp', 'fec_ing', 'tipo_sangre']
             })
             .fromFile(file_asegurados)
             .then((jsonObj) => {
+                    // ... error checks
                     jsonObj.forEach((e) => {
-                        request.query(`if not exists (select 1 from test_ase where cod_asegurado = '${e.cod_asegurado}')
-                                            insert into test_ase values(${parseInt(e.agenda)},'${e.cod_asegurado}','${e.nombre}','${e.fec_nac}','${e.sexo}',
-                                            '${e.ci}','${e.ci_loc}',${parseInt(e.cod_emp)},'${e.nom_emp}','${e.fec_ing}','${e.tipo_sangre}')
-                                        else
-                                            update test_ase set agenda=${parseInt(e.agenda)},cod_asegurado='${e.cod_asegurado}',nombre='${e.nombre}',
-                                            fec_nac='${e.fec_nac}',sexo='${e.sexo}',ci='${e.ci}',ci_loc='${e.ci_loc}',cod_emp=${parseInt(e.cod_emp)},
-                                            nom_emp='${e.nom_emp}',fec_ing='${e.fec_ing}'
-                                            where cod_asegurado = '${e.cod_asegurado}'`)
-                    }
-                );
+                        table.rows.add(parseInt(e.agenda), e.cod_asegurado, e.nombre, e.fec_nac, e.sexo, e.ci, e.ci_loc,
+                         parseInt(e.cod_emp), e.nom_emp, e.fec_ing, e.tipo_sangre)
+                        // request.query(`if not exists (select 1 from test_ase where cod_asegurado = '${e.cod_asegurado}')
+                        //                     insert into test_ase values(${parseInt(e.agenda)},'${e.cod_asegurado}','${e.nombre}','${e.fec_nac}','${e.sexo}',
+                        //                     '${e.ci}','${e.ci_loc}',${parseInt(e.cod_emp)},'${e.nom_emp}','${e.fec_ing}','${e.tipo_sangre}')
+                        //                 else
+                        //                     update test_ase set agenda=${parseInt(e.agenda)},cod_asegurado='${e.cod_asegurado}',nombre='${e.nombre}',
+                        //                     fec_nac='${e.fec_nac}',sexo='${e.sexo}',ci='${e.ci}',ci_loc='${e.ci_loc}',cod_emp=${parseInt(e.cod_emp)},
+                        //                     nom_emp='${e.nom_emp}',fec_ing='${e.fec_ing}'
+                        //                     where cod_asegurado = '${e.cod_asegurado}'`)
+                    });
             })
+            .then( async()=>{
+                await transaction.begin()
+                request.bulk(table,async (err, result) => {
+                    if (err) {
+                        console.log(err);
+                        await transaction.rollback();
+                        req.flash('aux', `${err}`);
+                        res.redirect('/actualizarBD');
+                    } else {
+                        console.log(result);
+                        req.flash('aux', `${result.rowsAffected} Asegurados insertados`);
+                        res.redirect('/actualizarBD');
+                    }
+                })
+            // table.rows.add(123, '123', '123', '123', '1', '123', '123',
+            //     123, '123', '123', '123')
+        })
     } catch (err) {
-        throw(err)
+        throw (err)
     }
 }
 
@@ -92,7 +135,7 @@ async function actualizarBD(req, res) {
             await poolConnect;
             console.log(req.body)
             if (req.body.btn_actualizarbd_ase == '') {
-                // updateAsegurados(req,res)
+                updateAsegurados(req, res)
             }
             if (req.body.btn_actualizarbd_bnf == '') {
                 // updateBeneficiarios(req,res)                
@@ -103,7 +146,7 @@ async function actualizarBD(req, res) {
     } catch (e) {
         req.flash('aux', e)
         res.redirect('/actualizarBD')
-        throw(e)
+        throw (e)
     }
 }
 
